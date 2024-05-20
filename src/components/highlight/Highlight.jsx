9d87f25a9747eff62f489e045dfad2152101482b
HugoQuintal01 <hugoquintal97@gmail.com>
2024-05-20 12:39:09 +0100

    Update hightlight section

--- src/components/highlight/Highlight.jsx
+++ src/components/highlight/Highlight.jsx
@@ -3,6 +3,10 @@ import House01 from '../../assets/house-03.jpeg';
 import House02 from '../../assets/house-04.jpeg';
 import House03 from '../../assets/house-05.jpeg';
 import House04 from '../../assets/house-06.jpeg';
+import House05 from '../../assets/house-07.jpeg';
+import House06 from '../../assets/house-08.jpeg';
+import House07 from '../../assets/house-09.jpeg';
+import House08 from '../../assets/house-10.jpeg';
 
 const Highlight = () => {
     return( 
@@ -69,6 +73,78 @@ const Highlight = () => {
                 <div className="highlight-item col-8 col-t-6 col-d-3">
                     <div className="highlight-item-image">
                         <img src={House04} />
+                    </div>
+                    <div className="highlight-item-info">
+                        <div className="highlight-item-title">
+                            <h4>Porto Residence</h4>
+                        </div>
+                        <div className="highlight-item-price">
+                            <span>179.000€</span>
+                        </div>
+                        <div className="highlight-item-more-info">
+                            <span>T3</span>
+                            <span>140 m2</span>
+                            <span>2 wc</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="highlight-item col-8 col-t-6 col-d-3">
+                    <div className="highlight-item-image">
+                        <img src={House05} />
+                    </div>
+                    <div className="highlight-item-info">
+                        <div className="highlight-item-title">
+                            <h4>Porto Residence</h4>
+                        </div>
+                        <div className="highlight-item-price">
+                            <span>179.000€</span>
+                        </div>
+                        <div className="highlight-item-more-info">
+                            <span>T3</span>
+                            <span>140 m2</span>
+                            <span>2 wc</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="highlight-item col-8 col-t-6 col-d-3">
+                    <div className="highlight-item-image">
+                        <img src={House06} />
+                    </div>
+                    <div className="highlight-item-info">
+                        <div className="highlight-item-title">
+                            <h4>Porto Residence</h4>
+                        </div>
+                        <div className="highlight-item-price">
+                            <span>179.000€</span>
+                        </div>
+                        <div className="highlight-item-more-info">
+                            <span>T3</span>
+                            <span>140 m2</span>
+                            <span>2 wc</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="highlight-item col-8 col-t-6 col-d-3">
+                    <div className="highlight-item-image">
+                        <img src={House07} />
+                    </div>
+                    <div className="highlight-item-info">
+                        <div className="highlight-item-title">
+                            <h4>Porto Residence</h4>
+                        </div>
+                        <div className="highlight-item-price">
+                            <span>179.000€</span>
+                        </div>
+                        <div className="highlight-item-more-info">
+                            <span>T3</span>
+                            <span>140 m2</span>
+                            <span>2 wc</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="highlight-item col-8 col-t-6 col-d-3">
+                    <div className="highlight-item-image">
+                        <img src={House08} />
                     </div>
                     <div className="highlight-item-info">
                         <div className="highlight-item-title">
